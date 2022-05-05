954466aff4afc666d2c40f6f5a156a8e9d3e7c79
Nigel <nigelsmyth@hotmail.com>
2022-05-05 12:32:34 +0100

    Added code to POST new data to an API

--- nextjs-calling-api/pages/comments/index.js
+++ nextjs-calling-api/pages/comments/index.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 function CommentsPage(){
     const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments')
@@ -9,8 +10,29 @@ function CommentsPage(){
         setComments(data)
     }
 
+    const submitComment = async () => {
+        const response = await fetch ('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({comment}),
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
     return (
         <>
+        <input 
+            type='text' 
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+        />
+
+        <button onClick={submitComment}>Submit comment</button>
+
+
         <button onClick = {fetchComments}>Load comments</button>
         {comments.map((comment) => {
             return (
